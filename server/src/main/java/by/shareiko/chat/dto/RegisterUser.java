2bd426455c17ee1f137a122761dfa34b602ac8e2
Pavel Shareiko <pavel.shareiko@gmail.com>
2023-02-22 01:21:21 +0300

    add validation for RegisterUser dto

--- server/src/main/java/by/shareiko/chat/dto/RegisterUser.java
+++ server/src/main/java/by/shareiko/chat/dto/RegisterUser.java
@@ -1,15 +1,25 @@
 package by.shareiko.chat.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
+
 @Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
+@Data
 public class RegisterUser {
+    @NotEmpty(message = "Username is required")
+    @Size(min = 3, message = "Username must be at least 3 characters")
     private String username;
+
+    @NotEmpty(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
+
+    @NotEmpty(message = "Email is required")
     private String firstName;
+
+    @NotEmpty(message = "Email is required")
     private String lastName;
 }
