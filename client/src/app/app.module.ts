8009143aa560c4fa40e612c921fb7d0930a77f5a
Gleb Kaidalik <gleb.kaidalik@gmail.com>
2023-02-23 23:49:33 +0300

    add login component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { MainComponent } from './layouts/main/main.component';
+import { LoginComponent } from './layouts/login/login.component';
 
 @NgModule({
   declarations: [
-    MainComponent
+    MainComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
