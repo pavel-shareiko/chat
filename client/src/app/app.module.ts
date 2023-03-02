cb7c7e552aeb2903b195f2a25fe48df9e5e4ad0c
Gleb Kaidalik <gleb.kaidalik@gmail.com>
2023-03-02 23:07:26 +0300

    added login page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { MainComponent } from './layouts/main/main.component';
-import { LoginComponent } from './layouts/login/login.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { MainComponent } from "./layouts/main/main.component";
+import { LoginComponent } from "./layouts/login/login.component";
 
 @NgModule({
-  declarations: [
-    MainComponent,
-    LoginComponent
-  ],
+  declarations: [MainComponent, LoginComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [MainComponent]
+  bootstrap: [MainComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- client/src/app/layouts/login/login.component.ts
+++ client/src/app/layouts/login/login.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { AuthService } from "src/app/auth/auth.service";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent {
+  form: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.form = this.fb.group({
+      login: ["", Validators.required],
+      password: ["", Validators.required],
+    });
+  }
+  login (){
+    const val = this.form.value;
+    console.log(val)
+    if (val.login && val.password) {
+      this.authService.login(val.login, val.password)
+          .subscribe(
+              () => {
+                  console.log("User is logged in");
+                  this.router.navigateByUrl('/chats');
+              }
+          );
+    }
+  }
 }
