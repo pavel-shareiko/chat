e00a9e0321a288c2e45ead58aca403ad9ef08290
Gleb Kaidalik <gleb.kaidalik@gmail.com>
2023-03-12 22:27:43 +0300

    add aplication config service

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,16 +6,16 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
 import { MainComponent } from "./layouts/main/main.component";
 import { LoginComponent } from "./layouts/login/login.component";
-import { ChatsComponent } from './layouts/chats/chats.component';
 import { AuthInterceptor } from "./auth/auth.interceptor";
-import { RegistrationComponent } from './layouts/registration/registration.component';
-import { NotFoundComponent } from './not-found/not-found.component';
+import { RegistrationComponent } from "./layouts/registration/registration.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
 
 @NgModule({
   declarations: [
-    MainComponent, 
-    LoginComponent, 
-    ChatsComponent, RegistrationComponent, NotFoundComponent
+    MainComponent,
+    LoginComponent,
+    RegistrationComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
@@ -24,7 +24,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
     HttpClientModule,
   ],
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
   ],
   bootstrap: [MainComponent],
 })
---
+++ client/src/app/config/application-config.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+  providedIn: "root",
+})
+export class ApplicationConfigService {
+  private endpointPrefix = "http://localhost:8080/";
+
+  setEndpointPrefix(endpointPrefix: string): void {
+    if (!endpointPrefix.endsWith("/")) {
+      endpointPrefix += "/";
+    }
+    this.endpointPrefix = endpointPrefix;
+  }
+
+  getEndpointFor(api: string): string {
+    if (api.startsWith("/")){
+      api = api.substring(1);
+    }
+    return `${this.endpointPrefix}${api}`;
+  }
+}
