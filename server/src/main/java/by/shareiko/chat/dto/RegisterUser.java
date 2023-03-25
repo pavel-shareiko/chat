67d340a0307b392f2c35c26ccf321b8c8b2e6529
Pavel Shareiko <pavel.shareiko@gmail.com>
2023-03-25 13:25:26 +0300

    add regex pattern for username and password

--- server/src/main/java/by/shareiko/chat/dto/RegisterUser.java
+++ server/src/main/java/by/shareiko/chat/dto/RegisterUser.java
@@ -2,6 +2,7 @@ package by.shareiko.chat.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
@@ -10,11 +11,11 @@ import lombok.*;
 @Data
 public class RegisterUser {
     @NotEmpty(message = "Username is required")
-    @Size(min = 3, message = "Username must be at least 3 characters")
+    @Pattern(regexp = "^[a-zA-Z\\d]{5,}$", message = "Username must be at least 5 characters and must not contain spaces")
     private String username;
 
     @NotEmpty(message = "Password is required")
-    @Size(min = 8, message = "Password must be at least 8 characters")
+    @Pattern(regexp = "^\\S{8,}$", message = "Username must be at least 5 characters and must not contain spaces")
     private String password;
 
     @NotEmpty(message = "Email is required")
