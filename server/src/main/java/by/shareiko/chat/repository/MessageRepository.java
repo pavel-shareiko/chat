a81cd8fd3c6e5e66d725c5bae7e0dae265ce9b81
Pavel Shareiko <pavel.shareiko@gmail.com>
2023-03-26 11:45:08 +0300

    fix errors when returning multiple results in getLastMessageInChat method

--- server/src/main/java/by/shareiko/chat/repository/MessageRepository.java
+++ server/src/main/java/by/shareiko/chat/repository/MessageRepository.java
@@ -1,6 +1,8 @@
 package by.shareiko.chat.repository;
 
 import by.shareiko.chat.domain.Message;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,13 +16,20 @@ public interface MessageRepository extends JpaRepository<Message, Long> {
 
     @Query("select m " +
            "from Message m " +
-           "where m.chat.id = :chatId")
-    List<Message> getChatMessages(@Param("chatId") Long chatId);
+           "where m.chat.id = :chatId " +
+           "order by m.createdAt " +
+           "desc")
+    List<Message> getAllChatMessages(@Param("chatId") Long chatId);
 
     @Query("select m " +
            "from Message m " +
            "where m.chat.id = :chatId " +
            "order by m.createdAt " +
            "desc")
-    Optional<Message> getLastMessageInChat(@Param("chatId") Long chatId);
+    List<Message> getChatMessages(@Param("chatId") Long chatId, Pageable pageable);
+
+    default Optional<Message> getLastMessageInChat(@Param("chatId") Long chatId) {
+        return getChatMessages(chatId, PageRequest.of(0, 1)).stream().findFirst();
+    }
+
 }
