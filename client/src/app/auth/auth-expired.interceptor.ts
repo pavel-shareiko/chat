4db90e6d6e4a20154d21b0dddd97e75d1accbade
Gleb Kaidalik <gleb.kaidalik@gmail.com>
2023-04-08 21:37:05 +0300

    modify auth interceptor

---
+++ client/src/app/auth/auth-expired.interceptor.ts
@@ -0,0 +1,37 @@
+import { Injectable } from "@angular/core";
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpErrorResponse,
+} from "@angular/common/http";
+import { Observable } from "rxjs";
+import { tap } from "rxjs/operators";
+import { Router } from "@angular/router";
+
+import { AuthService } from "./auth.service";
+@Injectable()
+export class AuthExpiredInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    console.log(request)
+    return next.handle(request).pipe(
+      tap({
+        error: (err: HttpErrorResponse) => {
+          if (
+            err.status === 401 &&
+            err.url
+          ) {
+            this.authService.logout();
+            this.router.navigate(["/login"]);
+          }
+        },
+      })
+    );
+  }
+}
