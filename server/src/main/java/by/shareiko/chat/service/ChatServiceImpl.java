ba2aa04348fd097afd9a311e6f5ed57031647133
Pavel Shareiko <pavel.shareiko@gmail.com>
2023-04-08 16:33:53 +0300

    return chats ordered by last message date, remove current user from chat participants

--- server/src/main/java/by/shareiko/chat/service/ChatServiceImpl.java
+++ server/src/main/java/by/shareiko/chat/service/ChatServiceImpl.java
@@ -3,14 +3,18 @@ package by.shareiko.chat.service;
 import by.shareiko.chat.domain.Chat;
 import by.shareiko.chat.domain.Message;
 import by.shareiko.chat.dto.ExtendedChatDTO;
+import by.shareiko.chat.dto.SimpleUserDTO;
 import by.shareiko.chat.exception.BadRequestException;
 import by.shareiko.chat.mapper.ChatMapper;
 import by.shareiko.chat.repository.ChatRepository;
+import by.shareiko.chat.security.SecurityUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.util.comparator.Comparators;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -28,11 +32,23 @@ public class ChatServiceImpl implements ChatService {
     public List<ExtendedChatDTO> getCurrentUserChatsWithLastMessage() {
         List<Chat> currentUserChats = getCurrentUserChats();
         return currentUserChats.stream()
-                .map(chat -> {
-                    Optional<Message> lastMessageOptional = chatMessageService.getLastMessageInChat(chat.getId());
-                    return chatMapper.chatToChatDTO(chat, lastMessageOptional.orElse(null));
-                })
+                .map(this::getExtendedChat)
+                .sorted((o1, o2) -> Comparators.nullsLow().compare(o2.getLastMessage(), o1.getLastMessage()))
                 .toList();
+    }
+
+    private ExtendedChatDTO getExtendedChat(Chat chat) {
+        Message lastMessage = chatMessageService.getLastMessageInChat(chat.getId()).orElse(null);
+
+        ExtendedChatDTO extendedChatDTO = chatMapper.chatToExtendedChatDTO(chat, lastMessage);
+
+        String currentUsername = SecurityUtils.getCurrentUserLogin().orElse(null);
+        Set<SimpleUserDTO> participants = extendedChatDTO.getParticipants().stream()
+                .filter(participant -> !participant.getUsername().equals(currentUsername))
+                .collect(Collectors.toSet());
+        extendedChatDTO.setParticipants(participants);
+
+        return extendedChatDTO;
     }
 
     @Override
