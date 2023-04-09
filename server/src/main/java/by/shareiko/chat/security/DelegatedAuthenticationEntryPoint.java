ad221d26f5979d033935bc8e19d23220466567a9
Pavel Shareiko <pavel.shareiko@gmail.com>
2023-04-09 14:30:12 +0300

    fix error messages

--- server/src/main/java/by/shareiko/chat/security/DelegatedAuthenticationEntryPoint.java
+++ server/src/main/java/by/shareiko/chat/security/DelegatedAuthenticationEntryPoint.java
@@ -2,22 +2,19 @@ package by.shareiko.chat.security;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import java.io.IOException;
 
 @Component("delegatedAuthenticationEntryPoint")
 public class DelegatedAuthenticationEntryPoint implements AuthenticationEntryPoint {
-    private final HandlerExceptionResolver resolver;
-
-    public DelegatedAuthenticationEntryPoint(@Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver) {
-        this.resolver = resolver;
-    }
-
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) {
-        resolver.resolveException(request, response, null, authException);
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        response.sendError(
+                HttpServletResponse.SC_UNAUTHORIZED,
+                authException.getMessage()
+        );
     }
 }
