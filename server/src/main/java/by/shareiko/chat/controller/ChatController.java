feca07041d4c3e9b00d31cbc40e11187e5e0c646
Pavel Shareiko <pavel.shareiko@gmail.com>
2023-04-09 16:52:51 +0300

    add API endpoint for creating a chat with user

--- server/src/main/java/by/shareiko/chat/controller/ChatController.java
+++ server/src/main/java/by/shareiko/chat/controller/ChatController.java
@@ -1,11 +1,12 @@
 package by.shareiko.chat.controller;
 
+import by.shareiko.chat.domain.Chat;
 import by.shareiko.chat.dto.ExtendedChatDTO;
 import by.shareiko.chat.service.ChatService;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,8 +21,14 @@ public class ChatController {
     }
 
     @GetMapping
-    public List<ExtendedChatDTO> getCurrentUserChats() {
+    public ResponseEntity<List<ExtendedChatDTO>> getCurrentUserChats() {
         log.debug("REST request to get current user chats");
-        return chatService.getCurrentUserChatsWithLastMessage();
+        return ResponseEntity.ok(chatService.getCurrentUserChatsWithLastMessage());
+    }
+
+    @PostMapping("/{username}")
+    public ResponseEntity<Chat> createChat(@PathVariable String username) {
+        log.debug("REST request to create chat with user {}", username);
+        return ResponseEntity.ok(chatService.startChat(username));
     }
 }
