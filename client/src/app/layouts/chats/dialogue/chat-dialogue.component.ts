701e43487d50a908c8c2c663d015e04d683cd9c3
Pavel Shareiko <pavel.shareiko@gmail.com>
2023-04-29 19:20:59 +0300

    add ability to send message to a chat

--- client/src/app/layouts/chats/dialogue/chat-dialogue.component.ts
+++ client/src/app/layouts/chats/dialogue/chat-dialogue.component.ts
@@ -6,20 +6,24 @@ import { MessagesService } from '../messages.service';
 import { DateFormatterService } from '../../../common/date-formatter.service';
 import { AccountService } from 'src/app/auth/account.service';
 import { IUser } from 'src/app/models/user.model';
-
 @Component({
   selector: 'app-chat-dialogue',
   templateUrl: './chat-dialogue.component.html',
   styleUrls: ['./chat-dialogue.component.scss'],
 })
 export class ChatDialogueComponent implements OnInit, AfterViewChecked {
-  @ViewChild('messageContainer') messageContainer!: ElementRef;
-  dialogueId!: number;
-  chatName: string = '';
-  messages: IMessage[] = [];
-  currentUser: IUser | null = null;
-  loadingMessages = true;
-  newMessage: string = '';
+  @ViewChild('messageContainer') private messageContainer!: ElementRef;
+  @ViewChild('myTextarea') myTextarea!: ElementRef;
+  public chatName: string = '';
+  public messages: IMessage[] = [];
+  public currentUser: IUser | null = null;
+  public loadingMessages = true;
+  public newMessage: string = '';
+  public rows: number = 1;
+  public textareaWidth: number = 0;
+  public maxHeight: string = '100px';
+
+  private dialogueId!: number;
 
   constructor(
     public dateFormatter: DateFormatterService,
@@ -83,10 +87,53 @@ export class ChatDialogueComponent implements OnInit, AfterViewChecked {
       },
     });
   }
+  onInput() {
+    const textarea = this.myTextarea.nativeElement;
+    const { scrollHeight, clientWidth } = textarea;
+    const maxHeight = parseInt(this.maxHeight);
+    const newMessageLength = this.newMessage.length;
+    const rowHeight = 20;
+    const prevHeight = parseInt(textarea.style.height);
+
+    if (clientWidth !== this.textareaWidth) {
+      textarea.style.height = 'auto';
+      this.textareaWidth = clientWidth;
+    }
+
+    if (scrollHeight > maxHeight) {
+      textarea.style.overflow = 'auto';
+    } else {
+      textarea.style.overflow = 'hidden';
+    }
+
+    textarea.style.height = 'auto';
+    const height = Math.min(scrollHeight, maxHeight);
+    textarea.style.height = height + 'px';
+
+    this.rows = Math.ceil(scrollHeight / rowHeight);
+
+    if (
+      (textarea.style.overflow === 'hidden' && parseInt(textarea.style.height) < prevHeight) ||
+      textarea.value.length < newMessageLength
+    ) {
+      textarea.style.height = 'auto';
+      this.rows = Math.ceil(textarea.scrollHeight / rowHeight);
+    }
+  }
 
   sendMessage(): void {
     if (!this.newMessage) {
       return;
     }
+    this.newMessage = '';
+    // this.messagesService.sendMessage(this.dialogueId, this.newMessage).subscribe({
+    //   next: () => {
+    //     this.newMessage = '';
+    //     this.loadMessages();
+    //   },
+    //   error: err => {
+    //     console.error(err);
+    //   },
+    // });
   }
 }
