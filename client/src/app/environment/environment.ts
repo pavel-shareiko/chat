084a4550516b91efe4969f98a746e6ee26537940
Pavel Shareiko <pavel.shareiko@gmail.com>
2023-05-08 18:24:03 +0300

    set localhost as server hostname

--- client/src/app/environment/environment.ts
+++ client/src/app/environment/environment.ts
@@ -3,11 +3,11 @@ import { RxStompConfig } from '@stomp/rx-stomp';
 export const environment = {
   applicationName: 'Chat',
   protocol: 'http',
-  host: '192.168.100.7',
+  host: 'localhost',
   port: 8080,
   production: false,
   stompConfig: {
-    brokerURL: 'ws://192.168.100.7:8080/ws',
+    brokerURL: 'ws://localhost:8080/ws',
     connectionTimeout: 10000,
     reconnectDelay: 5000,
   } as RxStompConfig,
