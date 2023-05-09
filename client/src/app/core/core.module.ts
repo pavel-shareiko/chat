8f9ae2003533b5b38096b473ee098cf159365f8c
Pavel Shareiko <pavel.shareiko@gmail.com>
2023-05-09 23:25:43 +0300

    add user panel to the header

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -5,8 +5,12 @@ import { rxStompServiceFactory } from '../shared/stomp/rx-stomp-service-factory'
 import { RxStompService } from '../shared/stomp/rx-stomp.service';
 import { AuthExpiredInterceptor } from './interceptors/auth-expired.interceptor';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
+import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
+  imports: [CommonModule, FontAwesomeModule, MatTooltipModule],
   declarations: [HeaderComponent],
   exports: [HeaderComponent],
   providers: [
