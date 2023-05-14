6cf808228674f127e28b52db16c6bb2e71c9fad5
Pavel Shareiko <pavel.shareiko@gmail.com>
2023-05-15 00:09:47 +0300

    fix bug with Edited label not being displayed

--- server/src/main/java/by/shareiko/chat/dto/AuthenticationResponse.java
+++ server/src/main/java/by/shareiko/chat/dto/AuthenticationResponse.java
@@ -1,10 +1,8 @@
 package by.shareiko.chat.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class AuthenticationResponse {
     private final String token;
 }
